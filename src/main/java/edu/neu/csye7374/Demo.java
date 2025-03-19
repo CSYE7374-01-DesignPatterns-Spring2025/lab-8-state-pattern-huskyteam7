a893e5ab77ebe71aab2a5cc0a438ce4b9ae5b19e
Santosh Reddy Mallugari <144721268+SantoshReddyNEU@users.noreply.github.com>
2025-03-18 20:48:50 -0400

    Add files via upload

---
+++ src/main/java/edu/neu/csye7374/Demo.java
@@ -0,0 +1,19 @@
+package edu.neu.csye7374;
+
+public class Demo {
+    public static void demo() {
+        Restaurant restaurant = new Restaurant();
+
+        System.out.println("Displaying Lunch Menu:");
+        restaurant.displayMenu();
+
+        System.out.println("\nSwitching to Dinner Menu:");
+        restaurant.switchToDinner();
+        restaurant.displayMenu();
+
+        System.out.println("\nSwitching to Optional Menu:");
+        restaurant.switchToOptional();
+        restaurant.displayMenu();
+    }
+    
+}
